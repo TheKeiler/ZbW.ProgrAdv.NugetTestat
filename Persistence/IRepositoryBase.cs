b836eb6b1f51742ffd3d68e24d09c221d946bb20
Patrick Keist <p.keist@hotmail.com>
2019-05-28 14:36:54 +0200

    Changed IRepositoryBase to new cs file from thomas

--- Persistence/IRepositoryBase.cs
+++ Persistence/IRepositoryBase.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace ZbW.ProgrAdv.NugetTestat.Persistence
+namespace GenericRepository
 {
     public interface IRepositoryBase<M>
     {
@@ -12,21 +12,25 @@ namespace ZbW.ProgrAdv.NugetTestat.Persistence
         /// <param name="pkValue">Wert des PrimaryKey</param>
         /// <returns>gefundenes Model-Objekt, ansonsten null</returns>
         M GetSingle<P>(P pkValue);
+
         /// <summary>
         /// Fügt das Model-Objekt zur Datenbank hinzu (Insert)
         /// </summary>
         /// <param name="entity">zu speicherndes Model-Object</param>
         void Add(M entity);
+
         /// <summary>
         /// Löscht das Model-Objekt aus der Datenbank (Delete)
         /// </summary>
         /// <param name="entity">zu löschendes Model-Object</param>
         void Delete(M entity);
+
         /// <summary>
         /// Aktualisiert das Model-Objekt in der Datenbank hinzu (Update)
         /// </summary>
         /// <param name="entity">zu aktualisierendes Model-Object</param>
         void Update(M entity);
+
         /// <summary>
         /// Gibt eine Liste von Model-Objekten vom Typ M zurück,
         /// die gemäss der WhereBedingung geladen wurden. Die Werte der
@@ -35,33 +39,37 @@ namespace ZbW.ProgrAdv.NugetTestat.Persistence
         /// (Verhinderung von SQL-Injection)
         /// </summary>
         /// <param name="whereCondition">WhereBedingung als string
-        /// z.B. "NetPrice > @netPrice and Active = @active and
-        /// Description like @desc</param>
+        /// z.B. "NetPrice > @netPrice and Active = @active and Description like @desc</param>
         /// <param name="parameterValues">Parameter-Werte für die Wherebedingung
         /// bspw: {{"netPrice", 10.5}, {"active", true}, {"desc", "Wolle%"}}</param>
         /// <returns></returns>
         List<M> GetAll(string whereCondition, Dictionary<string, object> parameterValues);
+
         /// <summary>
         /// Gibt eine Liste aller in der DB vorhandenen Model-Objekte vom Typ M zurück
         /// </summary>
         /// <returns></returns>
         List<M> GetAll();
+
+        //IQueryable<M> Query(string whereCondition, Dictionary<string, object> parameterValues);
+
         /// <summary>
         /// Zählt in der Datenbank die Anzahl Model-Objekte vom Typ M, die der
         /// Where-Bedingung entsprechen
         /// </summary>
         /// <param name="whereCondition">WhereBedingung als string
-        /// z.B. "NetPrice > @netPrice and Active = @active and
-        /// Description like @desc</param>
+        /// z.B. "NetPrice > @netPrice and Active = @active and Description like @desc</param>
         /// <param name="parameterValues">Parameter-Werte für die Wherebedingung
         /// bspw: {{"netPrice", 10.5}, {"active", true}, {"desc", "Wolle%"}}</param>
         /// <returns></returns>
         long Count(string whereCondition, Dictionary<string, object> parameterValues);
+
         /// <summary>
         /// Zählt alle Model-Objekte vom Typ M
         /// </summary>
         /// <returns></returns>
         long Count();
+
         /// <summary>
         /// Gibt den Tabellennamen zurück, auf die sich das Repository bezieht
         /// </summary>
