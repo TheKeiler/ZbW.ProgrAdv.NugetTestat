6ca0d0269709c27b1e2b6827b14c77a21da4ef36
Patrick Keist <p.keist@hotmail.com>
2019-06-10 17:15:15 +0200

    Implemented TreeBuilder

---
+++ Model/LocationNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ZbW.ProgrAdv.NugetTestat.Model
+{
+    public class LocationNode
+    {
+        public Location ValueObject { get; set; }
+        public LocationNode ParentNode { get; set; }
+        public List<LocationNode> ChildNodesList { get; set; }
+
+        public LocationNode()
+        {
+            this.ChildNodesList = new List<LocationNode>();
+        }
+        public LocationNode(Location valueObject)
+        {
+            this.ValueObject = valueObject;
+            this.ParentNode = null;
+            this.ChildNodesList = new List<LocationNode>();
+        }
+
+        public LocationNode(LocationNode parentNode, Location valueObject)
+        {
+            this.ValueObject = valueObject;
+            this.ParentNode = parentNode;
+            this.ChildNodesList = new List<LocationNode>();
+        }
+
+        public void AddChildNode(LocationNode childNode)
+        {
+            this.ChildNodesList.Add(childNode);
+        }
+    }
+
+}
+
---
+++ Model/TreeBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ZbW.ProgrAdv.NugetTestat.Model
+{
+    public class TreeBuilder
+    {
+        public LocationNode BuildTree(List<Location> locations)
+        {
+            if (locations == null) return new LocationNode();
+            var nodeList = locations.ToList();
+            var tree = FindTreeRoot(nodeList);
+            BuildTree(tree, nodeList);
+            return tree;
+        }
+
+        private void BuildTree(LocationNode locationNode, List<Location> descendants)
+        {
+            var children = descendants.Where(node => node.ParentId == locationNode.ValueObject.Id).ToArray();
+            foreach (var child in children)
+            {
+                var branch = Map(child);
+                locationNode.AddChildNode(branch);
+                descendants.Remove(child);
+            }
+            foreach (var branch in locationNode.ChildNodesList)
+            {
+                BuildTree(branch, descendants);
+            }
+        }
+
+        private LocationNode FindTreeRoot(List<Location> nodes)
+        {
+            var rootNodes = nodes.Where(node => node.ParentId == null);
+            if (rootNodes.Count() != 1) return new LocationNode();
+            var rootNode = rootNodes.Single();
+            nodes.Remove(rootNode);
+            return Map(rootNode);
+        }
+
+        private LocationNode Map(Location loc)
+        {
+            return new LocationNode
+            {
+                ValueObject = loc,
+            };
+        }
+    }
+}
